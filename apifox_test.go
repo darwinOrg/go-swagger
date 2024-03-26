96eaabf2e54783c34ac01430e5ed907a4b8e7a93
chenbiao <149027310@qq.com>
2024-03-26 11:11:08 +0800

    init

--- apifox_test.go
+++ apifox_test.go
@@ -1,7 +1,6 @@
 package swagger_test
 
 import (
-	dgctx "github.com/darwinOrg/go-common/context"
 	"github.com/darwinOrg/go-common/page"
 	"github.com/darwinOrg/go-common/result"
 	"github.com/darwinOrg/go-swagger"
@@ -17,27 +16,19 @@ func TestSyncToApifoxRequest(t *testing.T) {
 		Remark:       "测试get接口",
 		RouterGroup:  engine.Group("/test"),
 		RelativePath: "/get",
-		NonLogin:     true,
-		BizHandler: func(_ *gin.Context, ctx *dgctx.DgContext, request *wrapper.MapRequest) *result.Result[*result.Void] {
-			return result.SimpleSuccess()
-		},
 	})
 
 	wrapper.Post(&wrapper.RequestHolder[UserRequest, *result.Result[*page.PageList[*UserRequest]]]{
 		Remark:       "测试post接口",
 		RouterGroup:  engine.Group("/test"),
 		RelativePath: "post",
-		NonLogin:     true,
-		BizHandler: func(gc *gin.Context, ctx *dgctx.DgContext, request *UserRequest) *result.Result[*page.PageList[*UserRequest]] {
-			return result.Success[*page.PageList[*UserRequest]](nil)
-		},
 	})
 
 	swagger.SyncSwaggerToApifox(&swagger.SyncToApifoxRequest{
 		RequestApis: wrapper.GetRequestApis(),
 		ProjectId:   "3450238",
-		AccessToken: "APS-d4KgT80K2Wu89UAUc6r94NchTH6SJeFM",
-		//AccessToken:         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDIyNzIxLCJ0cyI6IjgwYjI4MmIzOTNkMGY2MmMiLCJpYXQiOjE2OTUyODkxOTI0NzF9.U5ly2UQ0rpTIO_zdh68_pGvw7PvkZlW3lwTbg-cWySU",
+		//AccessToken: "APS-d4KgT80K2Wu89UAUc6r94NchTH6SJeFM",
+		AccessToken:         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDIyNzIxLCJ0cyI6IjgwYjI4MmIzOTNkMGY2MmMiLCJpYXQiOjE2OTUyODkxOTI0NzF9.U5ly2UQ0rpTIO_zdh68_pGvw7PvkZlW3lwTbg-cWySU",
 		ApiOverwriteMode:    swagger.ApiOverwriteModeIgnore,
 		SchemaOverwriteMode: swagger.SchemaOverwriteModeIgnore,
 		SyncApiFolder:       false,
--- swagger_test.go
+++ swagger_test.go
@@ -1,7 +1,6 @@
 package swagger_test
 
 import (
-	dgctx "github.com/darwinOrg/go-common/context"
 	"github.com/darwinOrg/go-common/page"
 	"github.com/darwinOrg/go-common/result"
 	"github.com/darwinOrg/go-swagger"
@@ -17,20 +16,12 @@ func TestExportSwaggerFile(t *testing.T) {
 		Remark:       "测试get接口",
 		RouterGroup:  engine.Group("/test"),
 		RelativePath: "/get",
-		NonLogin:     true,
-		BizHandler: func(_ *gin.Context, ctx *dgctx.DgContext, request *wrapper.MapRequest) *result.Result[*result.Void] {
-			return result.SimpleSuccess()
-		},
 	})
 
 	wrapper.Post(&wrapper.RequestHolder[UserRequest, *result.Result[*page.PageList[*UserRequest]]]{
 		Remark:       "测试post接口",
 		RouterGroup:  engine.Group("/test"),
 		RelativePath: "post",
-		NonLogin:     true,
-		BizHandler: func(gc *gin.Context, ctx *dgctx.DgContext, request *UserRequest) *result.Result[*page.PageList[*UserRequest]] {
-			return result.Success[*page.PageList[*UserRequest]](nil)
-		},
 	})
 
 	swagger.ExportSwaggerFile(&swagger.ExportSwaggerRequest{
